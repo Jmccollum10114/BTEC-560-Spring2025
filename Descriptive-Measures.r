ea3dd843d3a456bbfec7ee1f2a63b6495cc08032
Jmccollum10114 <jmccollum@claflin.edu>
2025-01-28 17:14:52 -0500

    Add files via upload

---
+++ Descriptive-Measures.r
@@ -0,0 +1,31 @@
+#Author: Jaylin McCollum, Date: 01/28/2025, Purpose: Descriptive Measures Functions
+
+#Calculate Standard Deviation
+#Create Dummy Data Set
+
+ data <- c(1:8)
+> data
+[1] 1 2 3 4 5 6 7 8
+> data <- c(1:8)
+> print(data)
+[1] 1 2 3 4 5 6 7 8
+
+> data1 <- c("A","B","C")
+
+> sd(data)
+[1] 2.44949
+
+> data2 <- c(1:1000)
+> sd(data2)
+[1] 288.8194
+> 
+> #Calculate Mean
+> 
+> mean(data)
+[1] 4.5
+> 
+> #Calculate Median
+> median(data)
+[1] 4.5
+> 
+
