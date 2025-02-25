0f920cf83de0a5dfa54a4192951cc2bbcfcc5a1c
Jmccollum10114 <jmccollum@claflin.edu>
2025-02-25 18:17:12 -0500

    Add files via upload

---
+++ Corrrelation-Test.R
@@ -0,0 +1,36 @@
+# Author: McCollum, 2/20/2025, Purpose: Test the Correlation Analysis
+
+library(ggpubr)
+
+#Loading a dummy Dataset
+my_data<-mtcars
+my_data
+head(my_data)
+
+                   mpg cyl disp  hp drat    wt  qsec vs am gear carb
+Mazda RX4         21.0   6  160 110 3.90 2.620 16.46  0  1    4    4
+Mazda RX4 Wag     21.0   6  160 110 3.90 2.875 17.02  0  1    4    4
+Datsun 710        22.8   4  108  93 3.85 2.320 18.61  1  1    4    1
+Hornet 4 Drive    21.4   6  258 110 3.08 3.215 19.44  1  0    3    1
+Hornet Sportabout 18.7   8  360 175 3.15 3.440 17.02  0  0    3    2
+Valiant           18.1   6  225 105 2.76 3.460 20.22  1  0    3    1
+
+# Create a Correlation Hypothesis
+# Weight of car is inversely proportional to the miles per gallon travelled by the car
+
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+# Run Correlation Test
+res<- cor.test(my_data$wt, my_data$mpg, method = "pearson")
+res
+
+        Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
