3670059bdbd0aff996064bc3d49499c4f35e82c1
Jmccollum10114 <jmccollum@claflin.edu>
2025-04-02 14:35:04 -0400

    Add files via upload

--- Anova-Test.R
+++ Anova-Test.R
@@ -22,3 +22,18 @@ Just add row.names=FALSE to your write.csv(PATH) call: write.csv (PATH, paste(na
 df[,c(2,4)]
 
 write.csv (df[[PATH]], paste(names(df[PATH]), ".csv", sep=""), row.names=FALSE)
+
+# Load Libray GGPlot
+library(ggplot2)
+
+#Plot data using GGPlot2
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
+# Apply ANOVA fn
+anova_one_way <- aov(time~poison, data = df)
+summary(anova_one_way)
+            Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
